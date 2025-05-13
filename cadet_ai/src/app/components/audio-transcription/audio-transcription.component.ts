2f8b3430a452ec7058e65193b1ead952f35a7916
Sunil Kumar Pradhan <sunilkumar_pradhan@epam.com>
2025-05-13 14:02:56 +0530

    Added basic files

---
+++ cadet_ai/src/app/components/audio-transcription/audio-transcription.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-transcription',
+  imports: [],
+  templateUrl: './audio-transcription.component.html',
+  styleUrl: './audio-transcription.component.css'
+})
+export class AudioTranscriptionComponent {
+
+}
